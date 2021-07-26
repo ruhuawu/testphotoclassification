0a9f8d87788885c9567e3eb123629acaebccbd26
“Ruhua <ruhua.wu@grabtaxi.com>
2021-07-26 11:27:26 -0700

    [ABC-123] fix issue

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+config = {
+         'username': 'testimgclassification',
+
+         'password': '4oeKrWh4M58kHa02e7W5fXTu96tAsc4MP2J7iHZzcTGrtV4UvGQ7HLhryNSfHHn3WuU8IsdZQf5HecquDqPsVA==',
+
+         'contactPoint': 'testimgclassification.cassandra.cosmos.azure.com',
+
+         'port': 10350
+         }
