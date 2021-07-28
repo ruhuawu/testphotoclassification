2c5df036790492827deb0ab2db0a9ad23a89a0c9
“Ruhua <ruhua.wu@grabtaxi.com>
2021-07-28 14:19:53 -0700

    [ABC-123] apply the object detection model

--- application.py
+++ application.py
@@ -1,45 +1,24 @@
 import os
-from threading import Thread
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template
-from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
-from keras.models import Sequential, load_model
-from werkzeug.utils import secure_filename
-import numpy as np
-from keras.applications.vgg16 import VGG16, decode_predictions
+from flask import Flask, request,  send_from_directory, render_template
 import uuid
 from cassandraCluster import cassandraCluster as dbcluster
 import json
+import potholeDetector as detector
 
-ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
-IMAGE_SIZE = (224, 224)
 UPLOAD_FOLDER = 'uploads'
-vgg16 = VGG16(weights='imagenet')
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-
-def predict(file):  
-    img  = load_img(file, target_size=IMAGE_SIZE)
-    img = img_to_array(img)/255.0
-    img = np.expand_dims(img, axis=0)
-    preds = vgg16.predict(img)
-    vgg16.predict(img)
-    probs = decode_predictions(preds, top=3)[0]
-    return probs
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def savePatholeInfoInCassandra(pathole): 
+def savePatholeInfoInCassandra(file_path): 
     db = dbcluster(app)
     session = db.connection.connect()
     id = uuid.uuid1()
-    params = [id, "lonitude", "latitude" ,"imagepath"]   
+    params = [id, "longitude", "latitude" ,file_path]   
     session.execute("INSERT INTO images.pathole (patholeid, longitude, latitude, imagepath) VALUES (%s,%s,%s,%s)", params)                 
 
 def saveImageInLocal(pathole):
-    filename = secure_filename(pathole.filename)
+    filename = pathole.filename
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     pathole.save(file_path)
     return file_path
@@ -51,20 +30,18 @@ def template_test():
 @app.route('/', methods=['POST'])
 def predict_pathole_from_app():
     file = request.files['file'] 
-    file_path = saveImageInLocal(file)
-    print(file_path)
-    probs = predict(file)
-    output = {probs[0][1]:probs[0][2]}
-    return render_template("home.html", label=output, imagesource=file_path)
+    file_path = saveImageInLocal(file)  
+    detector.detectPathole(file_path, file_path, True)
+    return render_template("home.html", imagesource=file_path)
     
 @app.route('/predict', methods=['POST'])
 def predict_pathole_from_api():
-    file = request.files['file']             
-    probs = predict(file)
-    if probs[0][2] >0 :
-       savePatholeInfoInCassandra(file)
-       print(json.dumps(str(probs[0])))
-    return json.dumps(str(probs[0]))
+    file = request.files['file']  
+    file_path = saveImageInLocal(file)                    
+    probs = detector.detectPathole(file_path, file_path, False)  
+    if(probs[0] >= 0.5):
+        savePatholeInfoInCassandra(file_path)
+    return json.dumps(str(probs))
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
---
+++ potholeDetector.py
@@ -0,0 +1,91 @@
+import os
+import pathlib
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from six import BytesIO
+from object_detection.utils import config_util
+from object_detection.builders import model_builder
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+import matplotlib
+import matplotlib.pyplot as plt
+
+filenames = list(pathlib.Path('pothole/training/').glob('*.index'))
+
+filenames.sort()
+print(filenames)
+#recover our saved model
+pipeline_config = 'pothole/mobilenet_v2.config'
+#generally you want to put the last ckpt from training in here
+model_dir = str(filenames[-1]).replace('.index','')
+configs = config_util.get_configs_from_pipeline_file(pipeline_config)
+model_config = configs['model']
+detection_model = model_builder.build(
+      model_config=model_config, is_training=False)
+
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(
+      model=detection_model)
+ckpt.restore(os.path.join(str(filenames[-1]).replace('.index','')))
+
+label_map_path =  'pothole/labelmap.pbtxt' #configs['eval_input_config'].label_map_path
+label_map = label_map_util.load_labelmap(label_map_path)
+categories = label_map_util.convert_label_map_to_categories(
+    label_map,
+    max_num_classes=label_map_util.get_max_label_map_index(label_map),
+    use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+label_map_dict = label_map_util.get_label_map_dict(label_map, use_display_name=True)
+
+
+def get_model_detection_function(model):
+  """Get a tf.function for detection."""
+
+  @tf.function
+  def detect_fn(image):
+    """Detect objects in image."""
+
+    image, shapes = model.preprocess(image)
+    prediction_dict = model.predict(image, shapes)
+    detections = model.postprocess(prediction_dict, shapes)
+
+    return detections, prediction_dict, tf.reshape(shapes, [-1])
+
+  return detect_fn
+
+detect_fn = get_model_detection_function(detection_model)
+
+
+def load_image_into_numpy_array(path):
+    img_data = tf.io.gfile.GFile(path, 'rb').read()
+    image = Image.open(BytesIO(img_data))
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+    (im_height, im_width, 3)).astype(np.uint8)
+
+def detectPathole(image_path, file_path, storeimg):
+    image_np = load_image_into_numpy_array(image_path)
+    input_tensor = tf.convert_to_tensor(
+    np.expand_dims(image_np, 0), dtype=tf.float32)
+    detections, predictions_dict, shapes = detect_fn(input_tensor)
+    if(storeimg):
+      label_id_offset = 1
+      image_np_with_detections = image_np.copy()
+
+      viz_utils.visualize_boxes_and_labels_on_image_array(
+       image_np_with_detections,
+       detections['detection_boxes'][0].numpy(),
+       (detections['detection_classes'][0].numpy() + label_id_offset).astype(int),
+       detections['detection_scores'][0].numpy(),
+       category_index,
+       use_normalized_coordinates=True,
+       max_boxes_to_draw=200,
+       min_score_thresh=.5,
+       agnostic_mode=False,
+      ) 
+      plt.figure(figsize=(12,16))
+      plt.imshow(image_np_with_detections)
+      plt.savefig(file_path) 
+    return detections['detection_scores'][0].numpy()
+
